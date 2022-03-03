391b50659d811b9beaf39d926eb3538fd0ff5e66
kovacsbencet <kovacsbencet@gmail.com>
2022-03-04 00:27:31 +0100

    .

--- frontend/script.js
+++ frontend/script.js
@@ -1,61 +1,81 @@
-const loadEvent = async () => {
+//component
+const content = () => { 
     return `
     <form>
-        <input id="input1" name="input1" type="text" />
-        <input id="input2" name="input2" type="text" />
-        <input id="input3" name="input3" type="text" />
+        <label for="">First Name</label><br>
+        <input type="text" id="firstName" name="firstName" value=""><br>
+        <label for="">Last Name</label><br>
+        <input type="text" id="lastName" name="lastName" value=""><br>
+        <label for="">Email address</label><br>
+        <input type="email" id="email" name="email" value=""><br>
         <select name="animals" id="animals">
-            <option value="both5555">Both</option>
-            <option value="cats5555">Cats</option>
-            <option value="dogs5555">Dogs</option>
+            <option value="both55">Both</option>
+            <option value="cats55">Cats</option>
+            <option value="dogs55">Dogs</option>
         </select>
-        <button>Send</button>
-    </form>
-    `;
-};
-
-function loadEvent() {
-console.log("load");
-const rootElement = document.getElementById("root")
-
-rootElement.insertAdjacentHTML("beforeend", formHTML())
-
-const form = rootElement.querySelector("form")
-
-const inputList = document.querySelectorAll("input")
-console.log(typeof inputList);
-console.log(Array.isArray(inputList));
-Array.from(inputList).map(function(input){
-    input.addEventListener("input", function(e) {
-        console.log(e.target.value);
-    })
-})
-
-form.querySelector("select").addEventListener("input", function(e) {
-    console.log(e.target.value)
-})
-
-form.addEventListener("submit", function(e) {
-    e.preventDefault()
-    console.log(e.target);
-})
-
-const apiKey = "xBU1reLNxxtLF1YzZ9bFVlHeZvdDAImrqqbgFRX5"
-
-const requestedDate = "2022-02-22"
-
-const apod = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&${requestedDate}`)
-
-console.log(apod);
-
-const apodJson = await apod.json()
-console.log(apodJson);
-
-
-/* for (const input of inputList) {
-    input.addEventListener("input", function(e) {
-        console.log(e.target.value);
-    })
-} */
+        <button>Submit</button>
+     </form>
+    `
 }
-window.addEventListener("load", loadEvent);
+
+const loadEvent = async () => {
+
+    //inserting component into html
+    const rootElement = document.getElementById("root")
+    rootElement.insertAdjacentHTML('beforeend', content())
+
+
+    //input event
+    const form = rootElement.querySelector("form")
+
+    const inputFields = document.querySelectorAll('input')
+    console.log(typeof inputFields)
+    console.log(Array.isArray(inputFields))
+    
+    Array.from(inputFields).map(function(input) {
+        input.addEventListener('input', function (e) {
+            console.log(e.target.value)
+        })
+    })
+
+    form.querySelector("select").addEventListener('input', function (e) {
+        console.log(e.target.value)
+    })
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault()
+        console.log(e.target)
+    })
+
+    /* for (let input of inputFields) {
+        input.addEventListener('input', function (e) {
+            console.log(e.target.value)
+        })
+    } */
+
+    //Fetch variation 1
+    const nasaApiKey = `KPARPnsf40jGYXOw4vJzj0yyULp7GTn5LHDXjpQV`
+    const requestedDate = `2020-08-21`
+
+    const apod = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaApiKey}&date=${requestedDate}`)
+
+    // console.log(apod)
+
+    const apodJson = await apod.json()
+    // console.log(apodJson.explanation)
+
+    //Fetch variation 2
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaApiKey}&date=${requestedDate}`).then(
+        function (apodReponse) {
+            console.log(apodReponse)
+            apodReponse.json().then(
+                function (apodResponseJson) {
+                    console.log(apodResponseJson.explanation)
+                }
+            )
+        }
+    )
+
+}
+
+window.addEventListener('load', loadEvent)
