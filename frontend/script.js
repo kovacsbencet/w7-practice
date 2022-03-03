4c3121285b41817335537f3b56ed27d7c3e1d5e6
kovacsbencet <kovacsbencet@gmail.com>
2022-03-03 17:20:15 +0100

    .

--- frontend/script.js
+++ frontend/script.js
@@ -1,41 +1,72 @@
-function init(mathFunction) {
-
-
-    var a = 5;
-    var b = 6;
-
-    //console.log(c)
-
-    if (a < b){
-
-        let c = mathFunction(b, a)
-
-        /* (function () {
-            var c = b - a;
-            console.log(c)
-        })();   */
-        console.log(c)
-    } else {
-
-        let c = mathFunction(a, b)
-        /*(function () {
-            var c = a - b;
-            console.log(c)
-        })();    */ 
-        console.log(c)
-    };
-
+function loadEvent() {
     
-};
+    let rootElement = document.getElementById("root");
 
-const initC = (firstNumber, secondNumber) => {
-    return firstNumber - secondNumber;
-};
+    rootElement.insertAdjacentHTML("beforeend", `
+    
+    <div id="container">
+        <form action="">
+        <label class="labels" for ="username">Create your username!</label>
+        <br>
+        <input type="text" name="username" id="username">
+        <br>
+        <label class="labels" for ="email">Add your e-mail address!</label>
+        <br>
+        <input type="text" name="email" id="email">
+        <br>
+        <label class="labels" for ="password">Add your password!</label>
+        <br>
+        <input type="text" name="password" id="password">
+        <br>
+        <select id="animals" name="animals">
+            <option value="both">Both</option>
+            <option value="cat">Cat</option>
+            <option value="dog">Dog</option>
+        </select>
+        <br>
+        <button>Submit!</button>
+    <div>
+    `)
 
-const initD = (firstNumber, secondNumber) => {
-    return firstNumber * secondNumber;
-};
+    //const username = document.querySelector('#username').value;
+    //const email = document.querySelector('#email').value;
+    //const password = document.querySelector('#password').value;
 
+    //console.log(username);
+    //console.log(email);
+    //console.log(password);
 
+    const inputList = document.querySelectorAll("input")
 
-init(initD);
+    console.log(typeof inputList);
+    console.log(Array.isArray(inputList))
+
+    Array.from(inputList).map(function(input){
+        input.addEventListener("input", function(event){
+            console.log(event.target.value)
+        })
+    })
+
+    const form = rootElement.querySelector("form");
+
+    form.querySelector("select").addEventListener("input", function(event) {
+        console.log(event.target.value)
+    })
+    
+    form.addEventListener("submit", function(event){
+        event.preventDefault()
+        console.log(event.target);
+    })
+    
+    
+
+    /* 
+    for (const input of inputList) {
+        input.addEventListener("input", function(event){
+            console.log(event.target.value)
+        })
+    }
+    */
+
+}
+window.addEventListener("load", loadEvent)
